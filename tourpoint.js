ca643dbde2503fdca7f810b2590c4f79ffbb3b63
Titaree534 <124623514+Titaree534@users.noreply.github.com>
2023-04-26 15:55:28 +0700

    Add files via upload

---
+++ tourpoint.js
@@ -0,0 +1,80 @@
+var tourpoint = {
+    "type": "FeatureCollection",
+    "features": [
+      {
+        "type": "Feature",
+        "properties": {
+          "Name": "Bangkok",
+          "Type": "Culture",
+          "Level": 10
+        },
+        "geometry": {
+          "coordinates": [
+            100.4920065352299,
+            13.75391662092565
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "Name": "Kanchanaburi",
+          "Type": "Natural",
+          "Level": 7
+        },
+        "geometry": {
+          "coordinates": [
+            99.5314082193496,
+            14.021845731740271
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "Name": "Nakhonphathom",
+          "Type": "Sprcial",
+          "Level": 8
+        },
+        "geometry": {
+          "coordinates": [
+            100.0591879290875,
+            13.820331111856575
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "Name": "Suphan Buri",
+          "Type": "Culture",
+          "Level": 6
+        },
+        "geometry": {
+          "coordinates": [
+            100.11750612905581,
+            14.47008630843925
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "Name": "Pattaya",
+          "Type": "Nature",
+          "Level": 10
+        },
+        "geometry": {
+          "coordinates": [
+            100.88740595827755,
+            12.937616361368214
+          ],
+          "type": "Point"
+        }
+      }
+    ]
+  }
